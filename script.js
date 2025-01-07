3945407f1933672be0ed5eb1f6e50b41461d3b18
sld-web <sadeep.web@yahoo.com>
2025-01-07 21:46:44 +0530

    final

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const container = document.querySelector('#container');
 const resetButton = document.querySelector('#reset-button')
-const sizeButton = document.querySelector('#grid-size')
+const sizeButton = document.querySelector('#grid-size');
+const clearButton = document.querySelector('#clear');
 
 
 function createGrid(size){
     container.innerHTML = '';
-    let gridCont = size*50;
-    container.style.width = `${gridCont}px`;
-    container.style.height = `${gridCont}px`;
     for (let i = 0; i < size*size; i++){
         const div = document.createElement('div');
         div.classList.add('square');
+        div.style.width = `${960/size}px`;
+        div.style.height = `${960/size}px`;
         container.appendChild(div);
         let r = Math.floor(Math.random()*256);
         let g = Math.floor(Math.random()*256);
@@ -23,20 +23,36 @@ function createGrid(size){
 
 function gridReset (){
     resetButton.addEventListener('click', () => {
-        gridSize = 0;
+        gridSize = 16;
     createGrid(gridSize);
-    })
+    });
 }
 
 function changeSize (){
     sizeButton.addEventListener('click', () => {
-        let userinp = prompt("Enter the size of the grid");
-        gridSize = Number(userinp);
+        let userinp = Number(prompt("Enter the size of the grid (1 - 100)"));
+        if (userinp <= 100 && userinp >= 1){
+            gridSize = userinp;
+        }
+        else {
+            alert("please enter a number between 1 and 100");
+            gridSize = 16;
+        }
         createGrid(gridSize);
-    })
+    });
 }
 
-let gridSize = 15;
+function clearGrid (){
+    clearButton.addEventListener('click', () => {
+        const squares = document.querySelectorAll('.square');
+        squares.forEach(square => {
+            square.style.backgroundColor = '';
+        });
+    });
+}
+
+let gridSize = 16;
 gridReset();
 changeSize();
+clearGrid();
 createGrid(gridSize);
